c1249c3ee54c3f3d31382049ed37c507a12f3780
Sergey Kiba <serhiikiba@gmail.com>
2024-03-01 18:44:28 +0100

    added aside

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import MainApp from './pages/mainApp/MainApp';
 
+import styles from './app.module.css';
+
 function App() {
 	return (
-		<div className='App'>
+		<div className={styles['app']}>
 			<MainApp />
 		</div>
 	);
