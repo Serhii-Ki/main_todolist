664ff806a0ca4f622119a766761be06680a0f168
Kiba Sergey <serhiikiba@gmail.com>
2024-05-13 18:20:10 +0200

    added customInput and btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import CustomBtn from "./components/CustomBtn/CustomBtn.tsx";
+import CustomInput from "./components/CustomInput/CustomInput.tsx";
 
 function App() {
 
 
   return (
     <>
-
+      <CustomBtn title={'Name'} onClick={() => {}}/>
+      <CustomInput label={'input'} size={'small'}/>
     </>
   )
 }
---
+++ src/components/CustomInput/CustomInput.tsx
@@ -0,0 +1,13 @@
+import TextField, {TextFieldProps} from '@mui/material/TextField';
+
+type InputPropsType = {
+  label: string
+} & TextFieldProps
+
+function CustomInput(props: InputPropsType) {
+  return (
+      <TextField variant="outlined" {...props}/>
+  );
+}
+
+export default CustomInput;
