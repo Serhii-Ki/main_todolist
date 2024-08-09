d762e2b74ac719a494ca77fb324b8567bfe0df0a
Kiba Sergey <serhiikiba@gmail.com>
2024-08-09 15:50:32 +0200

    cleaned the project and started new

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-import {Box, LinearProgress} from "@mui/material";
-import {useAppSelector} from "./store/store.ts";
-import {getAppState,} from "./store/selectors.ts";
-import CustomAppBar from "./components/appBar/AppBar.tsx";
-import {Outlet} from "react-router-dom";
-import CustomSnackbar from "./components/snackbar/CustomSnackBar.tsx";
-
 function App() {
-  const appState = useAppSelector(getAppState);
-
-  return (
-    <>
-      <CustomAppBar/>
-      <Box position='relative'>
-        {appState.loadStatus === 'loading' && <LinearProgress color="success" style={{position: 'absolute', bottom: '0', left: '0', right: '0'}}/>}
-      </Box>
-      <Outlet/>
-      <CustomSnackbar/>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {Provider} from "react-redux";
-import {store} from "./store/store.ts";
-import {RouterProvider} from "react-router-dom";
-import {router} from "./route/router.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
+    <App />
   </React.StrictMode>,
-)
+);
