8e82059d75cb02392046cd2da415e9f8a94f6727
Kiba Sergey <serhiikiba@gmail.com>
2024-08-14 16:43:27 +0200

    added addItemForm

--- src/components/addItemForm/AddItemForm.tsx
+++ src/components/addItemForm/AddItemForm.tsx
@@ -1,10 +1,19 @@
 import Box from "@mui/material/Box";
 import CustomInput from "../customInput/CustomInput.tsx";
+import { IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
-function AddItemForm() {
+type Props = {
+  label: string;
+};
+
+function AddItemForm({ label }: Props) {
   return (
     <Box>
-      <CustomInput />
+      <CustomInput size="small" label={label} />
+      <IconButton aria-label="add item" size="small">
+        <AddIcon />
+      </IconButton>
     </Box>
   );
 }
--- src/pages/todoListPage/TodoListPage.tsx
+++ src/pages/todoListPage/TodoListPage.tsx
@@ -1,5 +1,19 @@
+import AddItemForm from "../../components/addItemForm/AddItemForm.tsx";
+import Box from "@mui/material/Box";
+import { Container } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
 function TodoListPage() {
-  return <div></div>;
+  return (
+    <Container>
+      <Box display="flex" alignItems="center" flexDirection="column" mt="50px">
+        <Typography variant="h3" gutterBottom>
+          Todo Lists
+        </Typography>
+        <AddItemForm label={"add new list"} />
+      </Box>
+    </Container>
+  );
 }
 
 export default TodoListPage;
